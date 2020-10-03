9ac50e5147388a6fbc9f6461e95fafaeb27dc2b7
Juan Parra <juandkpa@gmail.com>
2020-10-03 10:22:53 -0500

    speakers with render props

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -3,27 +3,18 @@ import SpeakerSearchBar from '../SpeakerSearchBar/SpeakerSearchBar';
 import SpeakersRenderProps from './SpeakersRenderProps';
 
 const Speakers = () => {
-    const speakers = [
-        {
-            imageSrc: 'speaker-component-1124',
-            name: 'Douglas Crockford'
-        },
-        {
-            imageSrc: 'speaker-component-1530',
-            name: 'Tamara Baker'
-        },
-        {
-            imageSrc: 'speaker-component-10803',
-            name: 'Eugene Chuvyrov'
-        }
-    ];
-
     return (
-        <div>
-            {speakers.map( ({ imageSrc, name }) => (
-              <img src={`/images/${imageSrc}.png`} alt={name} key={imageSrc}></img>)
-            )}
-        </div>
+        <SpeakersRenderProps>
+            {({ speakers }) => {
+                return (
+                    <div>
+                        {speakers.map(({ imageSrc, name }) => {
+                            return <img src={`/images/${imageSrc}.png`} alt={name} key={imageSrc}></img>;
+                        })}
+                    </div>
+                );
+            }}
+        </SpeakersRenderProps>
     );
 };
 
